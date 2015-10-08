c346af95bc16189390c075fafc23d2a618476a61
nurzamat <nurzamat@gmail.com>
2015-10-08 22:39:09 +0600

    work

--- Public/Arzymo/app/src/main/java/org/ananasit/arzymo/ProfileDetailActivity.java
+++ Public/Arzymo/app/src/main/java/org/ananasit/arzymo/ProfileDetailActivity.java
@@ -69,7 +69,7 @@ public class ProfileDetailActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_post_detail, menu);
+        getMenuInflater().inflate(R.menu.menu_profile_detail, menu);
         return true;
     }
 
